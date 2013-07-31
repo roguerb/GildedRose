7f1aaae1d06130b784024b5b77248a5c1ed82737
kixcereal <jeramy.couts@gmail.com>
2013-07-30 22:16:24 -0700

    WIP Commit, adding hashes

--- lib/gilded_rose/rose.rb
+++ lib/gilded_rose/rose.rb
@@ -1,4 +1,5 @@
 module GildedRose
+
   class Rose
 
     @items = []
@@ -13,51 +14,60 @@ module GildedRose
       @items << Item.new("Conjured Mana Cake", 3, 6)
     end
 
+    def logic(key, value)
+      hash = {
+        "+5 Dexterity Vest" => {
+          [1..10] => -1,
+          [-50..0] => -2
+        },
+        "Aged Brie" => {
+          [-50..2] => +1
+        },
+      }
+
+      puts "Hey!"
+
+      hash[key].each do
+        |k,v|
+        if k.include?(value)
+          return v
+        end
+      end
+
+      # case key
+      # when "+5 Dexterity Vest"
+      #   return -1
+      # when "Aged Brie"
+      #   return +1
+      # when "Elixir of the Mongoose"
+      #   case value
+      #   when value > 1
+      #     return -1
+      #   when value <= 1
+      #     return -2
+      #   end
+      # when "Sulfuras, Hand of Ragnaros"
+      #   return 0
+      # when "Backstage passes to a TAFKAL80ETC concert"
+      #   case value
+      #   when value > 10
+      #     return -1
+      #   when value <= 10 && value >= 5
+      #     return -2
+      #   when value < 5
+      #     return -3
+      #   end
+      # when "Conjured Mana Cake"
+      #   return -2
+      # end
+    end
+
     def update_quality
-      for i in 0..(@items.size-1)
-        if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-          if (@items[i].quality > 0)
-            if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-              @items[i].quality = @items[i].quality - 1
-            end
-          end
-        else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
-            if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-              if (@items[i].sell_in < 11)
-                if (@items[i].quality < 50)
-                  @items[i].quality = @items[i].quality + 1
-                end
-              end
-              if (@items[i].sell_in < 6)
-                if (@items[i].quality < 50)
-                  @items[i].quality = @items[i].quality + 1
-                end
-              end
-            end
-          end
-        end
-        if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-          @items[i].sell_in = @items[i].sell_in - 1;
-        end
-        if (@items[i].sell_in < 0)
-          if (@items[i].name != "Aged Brie")
-            if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-              if (@items[i].quality > 0)
-                if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                  @items[i].quality = @items[i].quality - 1
-                end
-              end
-            else
-              @items[i].quality = @items[i].quality - @items[i].quality
-            end
-          else
-            if (@items[i].quality < 50)
-              @items[i].quality = @items[i].quality + 1
-            end
-          end
-        end
+      logic("Aged Brie", 2);
+      @item.each do |x|
+        x.sell_in -= 1
+        number = logic(x.name, x.sell_in)
+        x.quality = (x.quality + number)
       end
     end
 
