ab5a52487c4621cc3db488223524d501356c407d
Luke Barbuto <luke.barbuto@gmail.com>
2013-08-11 15:25:57 -0700

    Finish adding essential operation conditions

--- lib/gilded_rose/operation.rb
+++ lib/gilded_rose/operation.rb
@@ -3,15 +3,34 @@ module GildedRose
     attr_reader :type, :attribute, :value, :conditions
 
     def initialize(type, attribute)
-      @type, @attribute, @conditions = type, attribute, {}
+      @type = type
+      @attribute = attribute
+      @conditions = {}
     end
 
     def by(value)
       @value = value
+      return self
+    end
+
+    def always
+      @conditions = false
+      return self
     end
 
     def when(condition)
       @conditions.merge! condition => :pending
+      return self
+    end
+
+    def less_than(value)
+      @conditions[@conditions.key(:pending)] = { less_than: value }
+      return self
+    end
+
+    def more_than(value)
+      @conditions[@conditions.key(:pending)] = { more_than: value }
+      return self
     end
   end
 end
--- spec/gilded_rose/operation_spec.rb
+++ spec/gilded_rose/operation_spec.rb
@@ -2,19 +2,64 @@ require 'spec_helper'
 
 module GildedRose
   describe Operation do
-  let(:operation) { Operation.new(:increase, :quality) }
+    subject { Operation.new(:increase, :quality) }
+
+    shared_examples "operation modifier method" do
+      it "returns the operation" do
+        expect(@return).to eql(subject)
+      end
+    end
 
     describe "#by" do
+      before { @return = subject.by(2) }
+      include_examples "operation modifier method"
+
       it "adds a value to the operation array" do
-        operation.by(2)
-        expect(operation.value).to eql(2)
+        expect(subject.value).to eql(2)
+      end
+    end
+
+    describe "always" do
+      before { @return = subject.always }
+      include_examples "operation modifier method"
+
+      it "sets conditions to false" do
+        expect(subject.conditions).to be_false
       end
     end
 
     describe "#when" do
-      it "adds a condition to the operation array" do
-        operation.when(:less_than)
-        expect(operation.conditions).to include(less_than: :pending)
+      before { @return = subject.when(:quality) }
+      include_examples "operation modifier method"
+
+      it "adds a pending condition to the operation array" do
+        expect(subject.conditions).to include(quality: :pending)
+      end
+    end
+
+    describe "#less_than" do
+      before do
+        subject.conditions.merge! quality: :pending
+        @return = subject.less_than(5)
+      end
+
+      include_examples "operation modifier method"
+
+      it "adds the labled value to the pending condition" do
+        expect(subject.conditions).to include(quality: {less_than: 5})
+      end
+    end
+
+    describe "#more_than" do
+      before do
+        subject.conditions.merge! quality: :pending
+        @return = subject.more_than(5)
+      end
+
+      include_examples "operation modifier method"
+
+      it "adds the labled value to the pending condition" do
+        expect(subject.conditions).to include(quality: {more_than: 5})
       end
     end
   end
