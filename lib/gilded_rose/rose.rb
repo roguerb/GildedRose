fc6e6c75449b5f380b2084e53283e1ad15c63d0a
Luke Barbuto <luke.barbuto@gmail.com>
2013-08-13 20:12:00 -0700

    Remove duplicate condition

--- lib/gilded_rose/rose.rb
+++ lib/gilded_rose/rose.rb
@@ -32,13 +32,11 @@ module GildedRose
 
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
         increase_quality(item)
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if (item.sell_in < 10)
-            increase_quality(item)
-          end
-          if (item.sell_in < 5)
-            increase_quality(item)
-          end
+        if (item.sell_in < 10)
+          increase_quality(item)
+        end
+        if (item.sell_in < 5)
+          increase_quality(item)
         end
       else
         decrease_quality(item)
